7327d4fb12e55946bd49adfa28c41eccfd6cc442
root <root@PATRYK-NFHTHTE0>
2023-09-09 09:55:46 -0400

    committing files for review

--- app.js
+++ app.js
@@ -1,132 +1,103 @@
-function randomRGB(){
+// Function to generate a random RGB color.
+function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`
+    return `rgb(${r},${g},${b})`;
+}
+
+// Function to change body background color based on mouse position.
+function changeBodyColorOnMouseMove(event) {
+    const r = Math.round(event.pageY * 255 / window.innerHeight);
+    const b = Math.round(event.pageX * 255 / window.innerWidth);
+    document.body.style.backgroundColor = `rgb(${r}, 0, ${b})`;
+}
+
+// Function to handle todo item interactions.
+function handleTodoItemClick(event) {
+    const clickedItem = event.target;
+    if (clickedItem.tagName === 'BUTTON') {
+        clickedItem.parentElement.remove();
+         // save to localStorage
+        savedTodos.pop({ todo: todoItem.value, isCompleted: false });
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+    } else if (clickedItem.tagName === 'LI') {
+        clickedItem.classList.add('todo-done');
+        clickedItem.isCompleted = true;
+        let index = savedTodos.find(todo => todo.tId === clickedItem.id);
+        index.isCompleted = true;
+        console.log(savedTodos);
+
+
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+    }
+    
 
 }
 
+// Function to handle addition of a new todo item.
+function handleTodoFormSubmit(event) {
+    event.preventDefault();
+
+    const newTodo = document.createElement('li');
+    const removeTodo = document.createElement('button');
+    const todoId = Math.random().toString(36).substring(2,9);
+
+    removeTodo.innerText = 'Remove Todo';
+    removeTodo.classList.add('todo-button');
+    
+    currentTodo = todoItem.value;
+    newTodo.innerText = currentTodo;
+    newTodo.isCompleted = false;
+    newTodo.id = todoId;
+
+    newTodo.appendChild(removeTodo);
+    todoList.appendChild(newTodo);
+
+    todoItem.value = ''; // Clear the input.
+
+    
+    // save to localStorage
+    savedTodos.push({ todo: currentTodo, isCompleted: false, tId: todoId });
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+}
+
+// DOM Element References
+const todoForm = document.querySelector('#todo-add');
+const todoItem = document.querySelector('#todo-item');
+const todoList = document.querySelector('#todo-list');
 const letters = document.querySelectorAll('.letter');
 
+// Load todos from localStorage and display them
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+for (let todo of savedTodos) {
+    const newTodo = document.createElement("li");
+    const removeTodo = document.createElement('button');
 
-setInterval(function(){
-    for (let letter of letters){
+    removeTodo.innerText = 'Remove Todo';
+    removeTodo.classList.add('todo-button');
+
+    newTodo.innerText = todo.todo;
+    newTodo.isCompleted = todo.isCompleted ? true : false;
+    newTodo.id = todo.tId;
+    console.log(todo.isCompleted)
+    
+    if (newTodo.isCompleted) {
+        newTodo.classList.add('todo-done');
+    }
+    todoList.appendChild(newTodo);
+    newTodo.appendChild(removeTodo);
+}
+
+// Add event listeners
+document.addEventListener("mousemove", changeBodyColorOnMouseMove);
+todoList.addEventListener('click', handleTodoItemClick);
+todoForm.addEventListener('submit', handleTodoFormSubmit);
+
+// Change the color of letters at a regular interval.
+setInterval(() => {
+    for (let letter of letters) {
         letter.style.color = randomRGB();
     }
-    
-}, 500)
-
-
-function makeBody(color){
-    document.body.style.backgroundColor = color;
-}
-const btn = document.querySelector('button.mack');
-btn.addEventListener("click", function (ev) {
-    makeBody('cyan');
-    console.log('Im clicke');
-    console.log(ev);
-})
-
-
-document.addEventListener("mousemove", function(ev){
-    let r = Math.round(ev.pageY*255/window.innerHeight);
-    let b = Math.round(ev.pageX*255/window.innerWidth);
-    document.body.style.backgroundColor = `rgb(${r}, 0, ${b})`;
-
-})
-
-
-const form = document.querySelector('#beaverform');
-const brandInput = document.querySelector('input[name="brandname"]');
-const colorInput = document.querySelector('input[name="color"]');
-const fontSizeInput = document.querySelector('input[name="fontsize"]');
-const results = document.querySelector('#results');
-
-form.addEventListener('submit', function(evt){
-    evt.preventDefault();
-    console.log(brandInput.value, colorInput.value, fontSizeInput.value);
-    const newLogo = makeLogo(
-        brandInput.value,
-        colorInput.value,
-        fontSizeInput.value
-    );
-    results.appendChild(newLogo);
-});
-
-function makeLogo(text,color, size){
-    const logo = document.createElement('h2');
-    
-    logo.innerText = text;
-    logo.style.color = color;
-    logo.style.fontSize = size + 'px';
-    return logo;
-
-}
-
-document.addEventListener("keypress", function(e){
-    console.log(e.key)
-})
-
-// const removeButtons = document.querySelectorAll('li button');
-// for(let btn of removeButtons){
-//     btn.addEventListener('click', function(e){
-//         e.target.parentElement.remove();
-
-// });
-
-
-// }
-
-// const beaverForm = document.querySelector('#add-friend');
-// const beaverInput = document.querySelector('#first-name');
-// const beaverList = document.querySelector('#beaver-list');
-
-// beaverForm.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     console.log(beaverInput.value);
-//     const newBeaver = document.createElement('li');
-//     const removeBeaverBtn = document.createElement('button');
-//     removeBeaverBtn.innerText = 'Unfriend';
-//     removeBeaverBtn.addEventListener('click', function(e){
-//         e.target.parentElement.remove();
-
-// });
-//     newBeaver.innerText = beaverInput.value;
-//     newBeaver.appendChild(removeBeaverBtn);
-//     beaverInput.value = '';
-//     beaverList.appendChild(newBeaver);
-
-
-// })
-
-
-
-const beaverForm = document.querySelector('#add-friend');
-const beaverInput = document.querySelector('#first-name');
-const beaverList = document.querySelector('#beaver-list');
-
-beaverList.addEventListener('click', function(e){
-    if(e.target.tagName === 'BUTTON'){
-        e.target.parentElement.remove();
-    } else if(e.target.tagName  === 'LI'){
-        e.target.classList.add('best-beaver');
-        const heart = document.createElement('span');
-        heart.innerHTML = '&hearts;';
-        e.target.prepend(heart);
-
-    }
-})
-
-beaverForm.addEventListener('submit', function(e){
-    e.preventDefault();
-    console.log(beaverInput.value);
-    const newBeaver = document.createElement('li');
-    const removeBeaverBtn = document.createElement('button');
-    removeBeaverBtn.innerText = 'Unfriend';
-    newBeaver.innerText = beaverInput.value;
-    newBeaver.appendChild(removeBeaverBtn);
-    beaverInput.value = '';
-    beaverList.appendChild(newBeaver);
-
-
-})
+}, 500);
